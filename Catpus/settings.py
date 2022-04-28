0e4b04a75c646fab5094bb64c168ea97e68858f4
GreatGu <guyuhan1999@126.com>
2022-04-28 15:22:01 +0800

    add static_root

--- Catpus/settings.py
+++ Catpus/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
